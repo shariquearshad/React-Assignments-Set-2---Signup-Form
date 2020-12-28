776e353828d19d20f1c7a661d1e084a77bfa2856
shariquearshad <shariquearshad@gmail.com>
2020-12-28 12:02:47 +0530

    final

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,151 @@
 import React, { useState } from "react";
-import SignUpForm from "./SignUpForm";
-import SuccessForm from "./SuccessForm";
 
-function Form() {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  /* const [emailValue, setEmailValue] = useState(""); */
-  let username = "";
+const Form = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    gender: "male",
+    phoneNumber: "",
+    password: ""
+  });
 
-  function submitForm(item) {
-    username = item;
-    console.log("inside submitForm: " + username);
-    setIsSubmitted(true);
-  }
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
 
-  /* function emailProfileHandler(item) {
-        setEmailValue({emailValue: item});
-        console.log("inside emailProfile: "+emailValue);
-    } */
+  const handleChange = ({ target }) => {
+    const key = target.getAttribute("name");
+    setState((prev) => ({
+      ...prev,
+      [key]: target.value
+    }));
+  };
+
+  const isEmpty = () => {
+    for (let key in state) {
+      if (state[key] === "") {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const isAlphaNumeric = () => {
+    let letters = /^[0-9a-zA-Z ]+$/;
+    return letters.test(state.name);
+  };
+
+  const isValidEmail = () => {
+    return state.email.includes("@");
+  };
+
+  const hasGender = () => {
+    return (
+      state.gender === "male" ||
+      state.gender === "female" ||
+      state.gender === "other"
+    );
+  };
+
+  const isValidPhoneNumber = () => {
+    let numbers = /^[0-9]+$/;
+    return numbers.test(state.phoneNumber);
+  };
+
+  const isValidPassword = () => {
+    return state.password.length >= 6;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isEmpty()) {
+      setError("All fields are mandatory");
+    } else if (!isAlphaNumeric()) {
+      setError("Name is not alphanumeric");
+    } else if (!hasGender()) {
+      setError("Please identify as male, female or others");
+    } else if (!isValidEmail()) {
+      setError("Email must contain @");
+    } else if (!isValidPhoneNumber()) {
+      setError("Phone Number must contain only numbers");
+    } else if (!isValidPassword()) {
+      setError("Password must contain atleast 6 letters");
+    } else {
+      setMessage(`Hello ${state.email.split("@")[0]}`);
+      setError(null);
+    }
+  };
+
   return (
     <div>
-      {!isSubmitted ? (
-        <SignUpForm
-          /* emailProfile = {emailProfileHandler} */ submitForm={submitForm}
-        />
-      ) : (
-        <SuccessForm username={username} />
-      )}
+      <h1>{error ? error : message}</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            value={state.name}
+            data-testid="name"
+            name="name"
+            type="text"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            name="email"
+            type="text"
+            value={state.email}
+            data-testid="email"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Gender:
+          <select
+            name="gender"
+            id="gender"
+            data-testid="gender"
+            value={state.gender}
+            onChange={handleChange}
+          >
+            <option value="male" selected={state.gender === "male"}>
+              Male
+            </option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Phone:
+          <input
+            name="phoneNumber"
+            type="text"
+            value={state.phoneNumber}
+            data-testid="phoneNumber"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            name="password"
+            value={state.password}
+            data-testid="password"
+            type="password"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button data-testid="submit" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default Form;
